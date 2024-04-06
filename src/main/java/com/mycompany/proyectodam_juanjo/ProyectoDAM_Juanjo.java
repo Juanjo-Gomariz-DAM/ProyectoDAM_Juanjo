c3a3faff8aa7b6971ddc1d256f27c17fcebe4596
Usuario <Usuario@DESKTOP-L5QFR0V>
2024-04-06 22:25:32 +0200

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/proyectodam_juanjo/ProyectoDAM_Juanjo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectodam_juanjo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoDAM_Juanjo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
